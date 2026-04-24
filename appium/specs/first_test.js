c5d52497f5ea16bf3173ef9acd16611fb5e40d31
Da Da <csd@reach52.com>
2026-04-24 21:37:30 +0630

    test

--- appium/specs/first_test.js
+++ appium/specs/first_test.js
@@ -1,8 +1,6 @@
-describe("My Android Automation", () => {
+describe("VBMS", () => {
   it("should verify the app is launched", async () => {
-    // This waits for the app to be visible
     const isAppVisible = await driver.isAppInstalled("com.example.myapp");
-    // Note: Change 'com.example.myapp' to your actual package name!
 
     console.log(`Is app installed? ${isAppVisible}`);
     await expect(isAppVisible).toBe(true);
